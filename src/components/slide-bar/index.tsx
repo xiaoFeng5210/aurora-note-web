ef84eb4eea92fec56f161181616bb6b2307787a4
Eadweard <804875728@qq.com>
2024-02-20 20:09:29 +0800

    fix styles

--- src/components/slide-bar/index.tsx
+++ src/components/slide-bar/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, type FC } from 'react'
-import { SlideBarItem, selectedStyle } from './styled'
+import { SlideBarItem, selectedStyle, SlideBarWrapper } from './styled'
 import { useSlideBarStore, setCurrentSelectKey } from '../../store'
+import { Button } from "@/components/ui/button"
+
 const SlideBar: FC = () => {
   const currentSelectKey = useSlideBarStore((state) => state.currentSelectKey)
   const mockData = [
@@ -15,8 +17,10 @@ const SlideBar: FC = () => {
   ]
   return (
     <div className='tw-w-[300px] tw-h-full tw-bg-[#FBFBFA] tw-px-2'>
-      <header className='tw-h-[37px]'>标题</header>
-      <ul className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
+      <header className='tw-py-2'>
+        <Button className='w-full'>新增</Button>
+      </header>
+      <SlideBarWrapper className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
         {
           mockData.map((item) => (
             <SlideBarItem
@@ -28,7 +32,7 @@ const SlideBar: FC = () => {
             </SlideBarItem>
           ))
         }
-      </ul>
+      </SlideBarWrapper>
     </div>
   )
 }
--- src/components/slide-bar/styled.tsx
+++ src/components/slide-bar/styled.tsx
@@ -1,14 +1,23 @@
 import Styled from 'styled-components';
 
-const ItemHeight = `28px`
+const ItemHeight = `32px`
 
 export const selectedStyle = {
   backgroundColor: `#F2F2F1`,
   color: '#37342F'
 }
-
+export const SlideBarWrapper = Styled.ul`
+font-size: 1rem;
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+gap: 2px;
+`
 export const SlideBarItem = Styled.li`
-overflow: hidden; white-space: nowrap; text-overflow: ellipsis;
+padding-left: 5px;
+border-radius: 4px;
+overflow: auto; white-space: nowrap; text-overflow: ellipsis;
 height: ${ItemHeight};
 line-height: ${ItemHeight};
 &:hover {
