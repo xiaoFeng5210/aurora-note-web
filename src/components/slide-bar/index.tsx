9c24eadce4f17a8ee228122f5ab93ad60e5e4c81
Eadweard <804875728@qq.com>
2024-02-20 19:31:43 +0800

    style: hover

--- src/components/slide-bar/index.tsx
+++ src/components/slide-bar/index.tsx
@@ -5,8 +5,8 @@ const SlideBar: FC = () => {
     <div className='tw-w-[300px] tw-h-full tw-bg-[#FBFBFA] tw-px-2'>
       <header className='tw-h-[37px]'>标题</header>
       <ul className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
-        <SlideBarItem className='tw-h-[27px] tw-leading-[27px]'>测试测试测试测试测试测试测试测试测试测试测试测试</SlideBarItem>
-        <SlideBarItem className='tw-h-[27px]'>测试测试测试。。。。。</SlideBarItem>
+        <SlideBarItem className=''>测试测试测试测试测试测试测试测试测试测试测试测试</SlideBarItem>
+        <SlideBarItem className=''>测试测试测试。。。。。</SlideBarItem>
       </ul>
     </div>
   )
--- src/components/slide-bar/styled.tsx
+++ src/components/slide-bar/styled.tsx
@@ -1,7 +1,11 @@
 import Styled from 'styled-components';
 
+const ItemHeight = `28px`
+
 export const SlideBarItem = Styled.li`
 overflow: hidden; white-space: nowrap; text-overflow: ellipsis;
+height: ${ItemHeight};
+line-height: ${ItemHeight};
 &:hover {
   background-color: #EDEDEB;
   cursor: pointer;
