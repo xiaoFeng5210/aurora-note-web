78d99fe46744c80b3c099836ea456aa251107712
Eadweard <804875728@qq.com>
2024-02-21 19:07:21 +0800

    fix styles

--- src/components/slide-bar/index.tsx
+++ src/components/slide-bar/index.tsx
@@ -8,11 +8,10 @@ import { variants, itemVariants } from './animate.tsx'
 
 const SlideBar: FC = () => {
   const currentSelectKey = useSlideBarStore((state) => state.currentSelectKey)
-
   return (
     <div className='tw-w-[300px] tw-h-full tw-bg-[#FBFBFA] tw-px-2'>
       <header className='tw-py-2'>
-        <Button className='w-full'>新增</Button>
+        <Button className='w-full'>+ 新增</Button>
       </header>
       <SlideBarWrapper as={motion.ul} variants={variants} initial="hidden" animate="visible" exit="hidden"
         className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
