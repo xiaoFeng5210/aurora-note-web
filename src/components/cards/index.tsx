713f0a4f653e41e080b5b2bac336686db4117c98
Eadweard <804875728@qq.com>
2024-02-22 22:28:10 +0800

    feature: card styled

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,9 +1,14 @@
-import type { FC } from 'react'
+import { useState, type FC } from 'react'
 import { Button } from '../ui/button'
 import { motion } from 'framer-motion'
-import {variants, h1Variants, pVariants, buttonVariants} from './animate'
+import { variants, h1Variants, pVariants, buttonVariants } from './animate'
+import { CardsContainer } from './styled'
 
 const Cards: FC = () => {
+  const [hasCards, setHasCards] = useState(false);
+  const add = () => {
+    setHasCards(false)
+  }
 
   const Tips = () => (
     <motion.div variants={variants} animate="visible" initial="hidden" exit="hidden" className=''>
@@ -14,14 +19,21 @@ const Cards: FC = () => {
         <motion.p>开始新建第一个小记📒吧～</motion.p>
       </motion.div>
       <motion.div variants={buttonVariants}>
-        <Button>+ 新建小记</Button>
+        <Button onClick={() => add()}>+ 新建小记</Button>
       </motion.div>
     </motion.div>
   )
 
   return (
     <div className='tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center'>
-      <Tips />
+      {
+        hasCards ?
+          <Tips />
+          :
+          <CardsContainer>
+            
+          </CardsContainer>
+      }
     </div>
   )
 }
