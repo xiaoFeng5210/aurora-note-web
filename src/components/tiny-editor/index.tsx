9559f79385e8ad1eb8d57273efdf107f8b8b852d
Eadweard <804875728@qq.com>
2024-02-23 16:26:44 +0800

    editor focus

--- src/components/tiny-editor/index.tsx
+++ src/components/tiny-editor/index.tsx
@@ -1,24 +1,24 @@
 import { forwardRef, type FC } from 'react'
-import { Editor, } from '@tinymce/tinymce-react'
+import { Editor } from '@tinymce/tinymce-react'
 
 interface TinyMceEditorProps {
   onInit?: () => void
 }
 
 const TinyMceEditor: FC = forwardRef<TinyMceEditorProps, any>(({ onInit }) => {
-  const init = () => {
-    onInit && onInit()
-    const editorDom = document.querySelector('.tox-tinymce') as HTMLDivElement
-    if (editorDom) {
-      editorDom.style.border = 'none'
-    }
-  }
 
   return (
     <Editor
       apiKey='8py6yu9o93scou0cje5pztqemt1voia60506ix4yc39am4o0'
-      onInit={(evt, editor) => init()}
-      initialValue="<p>This is the initial content of the editor.</p>"
+      onInit={(evt, editor) => {
+        const editorDom = document.querySelector('.tox-tinymce') as HTMLDivElement
+        if (editorDom) {
+          editorDom.style.border = 'none'
+        }
+        editor.focus()
+        onInit && onInit()
+      }}
+      initialValue="<h3>新建小记...</h3>"
       init={{
         statusbar: false,
         height: '100%',
@@ -30,8 +30,8 @@ const TinyMceEditor: FC = forwardRef<TinyMceEditorProps, any>(({ onInit }) => {
         ],
         toolbar: 'undo redo | blocks | ' +
           'bold italic forecolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
+          'alignright alignjustify | bullist numlist outdent indent | ',
+        // 'removeformat | help',
         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
       }}
     />
