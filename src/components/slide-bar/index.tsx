588c4f39657665355f05d97193db14e044137e4b
Eadweard <804875728@qq.com>
2024-02-23 13:54:59 +0800

    组件退出动画

--- src/components/slide-bar/index.tsx
+++ src/components/slide-bar/index.tsx
@@ -3,7 +3,7 @@ import { SlideBarItem, selectedStyle, SlideBarWrapper } from './styled'
 import { useSlideBarStore, setCurrentSelectKey, setIsShowSlideBar } from '../../store'
 import { Button } from "@/components/ui/button"
 import { mockData } from './mock.ts'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 import { variants, itemVariants, logoVariants, foldVariants, ComponentVariants } from './animate.tsx'
 import { ArrowLeftToLine } from 'lucide-react'
 
@@ -18,31 +18,39 @@ const SlideBar: FC<SlideBarProps> = () => {
   }
 
   return (
-    <motion.div variants={ComponentVariants} animate={isShowSlideBar ? "visible" : "hidden"} className='tw-w-[300px] tw-h-full tw-bg-[#FBFBFA] tw-px-2'>
-      <header className='tw-py-2 tw-w-full tw-inline-flex tw-items-center tw-cursor-pointer'>
-        <motion.img variants={logoVariants} whileHover="hover" whileTap="tap" src="/img/aurora_logo_main.png" alt="" width="50px" />
-        <span className='tw-ml-2 tw-text-[#6B6A66] tw-text-[1.3rem] tw-font-bold'>Aurora Note</span>
-        <motion.span onClick={() => setIsShowSlideBar(false)} variants={foldVariants} initial="hidden" whileTap="tap" whileHover="visible" className='tw-ml-auto'>
-          <ArrowLeftToLine color="#9A9996" />
-        </motion.span>
-      </header>
-      <SlideBarWrapper as={motion.ul} variants={variants} initial="hidden" animate="visible" exit="hidden"
-        className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
-        {
-          mockData.map((item) => (
-            <SlideBarItem
-              as={motion.li}
-              variants={itemVariants}
-              key={item.key}
-              style={currentSelectKey === item.key ? selectedStyle : {}}
-              onClick={() => selectContent(item)}
-            >
-              {item.title}
-            </SlideBarItem>
-          ))
-        }
-      </SlideBarWrapper>
-    </motion.div>
+    <AnimatePresence>
+      {
+        isShowSlideBar && (
+          <motion.div variants={ComponentVariants} animate="visible" initial="hidden" exit="hidden" className='tw-w-[300px] tw-h-full tw-bg-[#FBFBFA] tw-px-2'>
+            <header className='tw-py-2 tw-w-full tw-inline-flex tw-items-center tw-cursor-pointer'>
+              <motion.img variants={logoVariants} whileHover="hover" whileTap="tap" src="/img/aurora_logo_main.png" alt="" width="50px" />
+              <span className='tw-ml-2 tw-text-[#6B6A66] tw-text-[1.3rem] tw-font-bold'>Aurora Note</span>
+              <motion.span onClick={() => setIsShowSlideBar(false)} variants={foldVariants} initial="hidden" whileTap="tap" whileHover="visible" className='tw-ml-auto'>
+                <ArrowLeftToLine color="#9A9996" />
+              </motion.span>
+            </header>
+            <SlideBarWrapper as={motion.ul} variants={variants} initial="hidden" animate="visible" exit="hidden"
+              className='tw-w-full tw-h-full tw-text-[1rem] tw-text-[#6B6A66] tw-text-left'>
+              {
+                mockData.map((item) => (
+                  <SlideBarItem
+                    as={motion.li}
+                    variants={itemVariants}
+                    key={item.key}
+                    style={currentSelectKey === item.key ? selectedStyle : {}}
+                    onClick={() => selectContent(item)}
+                  >
+                    {item.title}
+                  </SlideBarItem>
+                ))
+              }
+            </SlideBarWrapper>
+          </motion.div>
+        )
+      }
+
+    </AnimatePresence>
+
   )
 }
 export default SlideBar
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,16 @@ import { type FC } from 'react'
 import SlideBar from '@/components/slide-bar'
 import { Outlet } from 'react-router-dom'
 import { useSlideBarStore } from '@/store'
+import { motion } from 'framer-motion'
 
 const Home: FC = () => {
+  const isShowSlideBar = useSlideBarStore((state) => state.isShowSlideBar)
   return (
     <div className='tw-w-screen tw-h-dvh tw-overflow-hidden tw-flex tw-flex-row'>
       <SlideBar />
-      <main className='tw-flex-1 tw-p-4 tw-flex tw-flex-col'>
+      <motion.main layout className='tw-flex-1 tw-p-4 tw-flex tw-flex-col'>
         <Outlet />
-      </main>
+      </motion.main>
     </div>
   )
 }
