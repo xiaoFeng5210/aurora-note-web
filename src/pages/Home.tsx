ae4d978ce83c4dc7c818d1e0ef59ed994d0f9db9
Eadweard <804875728@qq.com>
2024-02-27 10:36:57 +0800

    重定向到home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
-import { type FC } from 'react'
+import { useEffect, type FC } from 'react'
 import SlideBar from '@/components/slide-bar'
 import { Outlet } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 
 const Home: FC = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/home')
+  }, [])
+  
   return (
     <div className='tw-w-screen tw-h-dvh tw-overflow-hidden tw-flex tw-flex-row'>
       <SlideBar />
